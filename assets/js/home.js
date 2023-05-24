8778e06b861838c2211fe64e99b64231eac4a166
SergAlKoba <Serg.Al.Koba@gmail.com>
2023-05-24 13:53:05 +0300

    lessen 24.05.2023

---
+++ assets/js/home.js
@@ -0,0 +1,24 @@
+$('.banner_slider').slick({
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    nextArrow: '<button class="slick-arrow next"><img src="../img/arrow_right.svg"></button>',
+    prevArrow: '<button class="slick-arrow prev"><img src="../img/arrow_left.svg"></button>',
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        }
+    ]
+});
