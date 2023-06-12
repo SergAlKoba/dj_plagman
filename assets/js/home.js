213361fab1963687cba26a4c425d70b31abc5f9e
Serhii Tsytsora <novochervone@gmail.com>
2023-06-12 13:02:44 +0300

    update 0.3

--- assets/js/home.js
+++ assets/js/home.js
@@ -22,3 +22,29 @@ $('.banner_slider').slick({
         }
     ]
 });
+
+$('.courses_slider').slick({
+  slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: '<button class="slick-arrow next"><img src="../img/arrow_right.svg"></button>',
+    prevArrow: '<button class="slick-arrow prev"><img src="../img/arrow_left.svg"></button>',
+    responsive: [
+        {
+          
+          breakpoint: 1024,
+          settings: {
+            arrows: false,
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+    ]
+})
